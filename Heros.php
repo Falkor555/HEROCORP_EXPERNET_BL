16cab4ddb45893c1328b230fe94ef29a64d0e574
Falkor555 <loic.botsy@hotmail.com>
2025-10-24 10:26:21 +0400

    Création et alimentation du front avec les fichiers hero-corp.php et Heros.php (la class)

---
+++ Heros.php
@@ -0,0 +1,111 @@
+<?php
+
+class Heros
+{
+    private $id;
+    private $nom;
+    private $prenom;
+    private $pseudo;
+    private $capacite;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * @param mixed $nom
+     */
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPrenom()
+    {
+        return $this->prenom;
+    }
+
+    /**
+     * @param mixed $prenom
+     */
+    public function setPrenom($prenom)
+    {
+        $this->prenom = $prenom;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPseudo()
+    {
+        return $this->pseudo;
+    }
+
+    /**
+     * @param mixed $pseudo
+     */
+    public function setPseudo($pseudo)
+    {
+        $this->pseudo = $pseudo;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCapacite()
+    {
+        return $this->capacite;
+    }
+
+    /**
+     * @param mixed $capacite
+     */
+    public function setCapacite($capacite)
+    {
+        $this->capacite = $capacite;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEquipe()
+    {
+        return $this->equipe;
+    }
+
+    /**
+     * @param mixed $equipe
+     */
+    public function setEquipe($equipe)
+    {
+        $this->equipe = $equipe;
+    }
+    private $equipe;
+
+    public function __construct()
+    {
+    }
+}
---
+++ hero-corp.php
@@ -0,0 +1,83 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.datatables.net/2.3.4/css/dataTables.dataTables.css" />
+
+</head>
+<body>
+<form action="bdd2.php" method="get">
+    <label for="search">Rechercher</label>
+    <input type="text" name="search" id="search">
+    <input type="submit" value="Search" >
+</form>
+<table id="myTable" class="display" style="width:100%">
+    <thead>
+    <tr>
+        <th>Id</th>
+        <th>Nom</th>
+        <th>Prénom</th>
+        <th>Pseudo</th>
+        <th>Capacité</th>
+        <th>Équipe</th>
+        <th>Action</th>
+    </tr>
+    </thead>
+
+    <tbody>
+    <?php
+    require('Heros.php');
+    $user="root";
+    $pass="";
+    $dbname="herocorploic";
+    $host="localhost";
+
+    $db=new PDO("mysql:host=$host;dbname=$dbname",$user,$pass);
+    $requete="";
+
+    if(isset($_GET['search'])){
+        $requete=$db->prepare("select * from heros 
+         where nom like ?  or 
+         prenom like ?");
+        $valeur="%".$_GET['search']."%";
+        $requete->bindParam(1,$valeur);
+        $requete->bindParam(2, $valeur);
+        $requete->execute();
+
+    }
+    else $requete=$db->query("select * from heros");
+
+    $requete->setFetchMode(PDO::FETCH_CLASS,'Heros');
+
+    $heros=$requete->fetchAll();
+
+
+    foreach ($heros as $hero) {
+
+        echo "<tr>
+    <td>".$hero->getId()."</td> 
+    <td>".$hero->getNom()."</td> 
+    <td>".$hero->getPrenom()."</td> 
+    <td>".$hero->getPseudo()."</td> 
+    <td>".$hero->getCapacite()."</td>
+    <td>".$hero->getEquipe()."</td>
+    <td><a href='modifier.php?id=".$hero->getId()."'>Modifier</a> | <a href='supprimer.php?id=".$hero->getId()."'>Supprimer</a></td>
+</tr>";
+    }
+    ?>
+
+    </tbody>
+
+</table>
+
+
+
+<script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+<script src="https://cdn.datatables.net/2.3.4/js/dataTables.js"></script>
+
+</body>
+</html>
+
+
